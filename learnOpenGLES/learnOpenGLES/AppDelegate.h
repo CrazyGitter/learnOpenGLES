a9be0c1cac975633563c23b79c177afaed0f1562
PengEhan <PengEhan@163.com>
2018-02-27 20:20:10 +0800

    初始化

---
+++ learnOpenGLES/learnOpenGLES/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  learnOpenGLES
+//
+//  Created by huya on 2018/2/27.
+//  Copyright © 2018年 pengyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ learnOpenGLES/learnOpenGLES/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  learnOpenGLES
+//
+//  Created by huya on 2018/2/27.
+//  Copyright © 2018年 pengyh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
